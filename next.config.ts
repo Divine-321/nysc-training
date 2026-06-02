1227e7128642cd0daad202ab2128c9c7228d3b95
Favour Edward <divinexcell321@gmail.com>
2026-06-02 19:00:48 +0100

    merged

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+  
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
